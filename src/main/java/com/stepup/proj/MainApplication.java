a6b8c76c86f9622a289287db4923847306772b07
Roman Grishin <grishin655@gmail.com>
2025-11-16 14:39:59 +0300

    Изменена основная логика Добавлен класс коробки

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,12 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("1111111");
+        System.out.println("1000001");
     }
+
+
+    public static void doSomething() {
+        System.out.println(1);
+    }
+
 }
