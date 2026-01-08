41fdfdb5cf0930147abf53b78a8fe6caedca3ef5
Roman Grishin <grishin655@gmail.com>
2026-01-08 12:37:49 +0300

    Добавлен класс Student для задания ООП №3 Студент, удалены неиспользьзумые классы для этой задачи

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -1,19 +1,22 @@
 package com.stepup.proj;
 
+import java.util.List;
+
 public class MainApplication {
     public static void main(String[] args) {
-        Square sq = new Square(-12, 15, 25);
-        System.out.println(sq);
+        Student student = new Student("Роман Гришин", List.of(4,5,2,3));
+        Student student1 = new Student("Петр Петрович", List.of(5,5,5,4));
+        System.out.println(student);
+        System.out.println(student1);
 
-        sq.changePoint(22, 30);
-        System.out.println(sq);
+        student.addGrade(4);
+        student.addGrade(2);
+        System.out.println(student);
 
-        sq.changePoint(0, -30);
-        sq.changeSideLength(500);
-        System.out.println(sq);
+        student.addGrade(5);
+        System.out.println(student.getGrades());
 
-        sq.changePoint(-15, 0);
-        sq.changeSideLength(-200);
-        System.out.println(sq);
+        student.addGrade(6);
+        System.out.println(student);
     }
 }
