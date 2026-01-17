844c735c137c40868dae58d789a6d45d760f2073
Roman Grishin <grishin655@gmail.com>
2026-01-17 19:24:10 +0300

    Разработан метод sumAll. Практика ООП. Полиморфизм #6. Сложение. Решение задачи.

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,15 +2,18 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
+        double res1 = sumAll(2, new Fraction(3,5), 2.3);
+        double res2 = sumAll(3.6, new Fraction(49,12), 3, new Fraction(3,2));
+        double res3 = sumAll(new Fraction(1,3), 1);
 
-        System.out.println(sumAll(2, new Fraction(3,5), 2.3));
-        System.out.println(sumAll(3.6, new Fraction(49,12), 3, new Fraction(3, 2)));
-        System.out.println(String.format("%.3f",sumAll(new Fraction(1,3), 1)));
+        System.out.println(String.format("%.3f",res1));
+        System.out.println(String.format("%.3f",res2));
+        System.out.println(String.format("%.3f",res3));
     }
 
-    public static double sumAll(Number... numbers){
+    public static double sumAll(Number... numbers) {
         double sum = 0;
-        for (Number number : numbers){
+        for (Number number : numbers) {
             sum += number.doubleValue();
         }
         return sum;
