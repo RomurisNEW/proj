4ab1d3da73792415de926fdac67da508d7848cfb
Roman Grishin <grishin655@gmail.com>
2026-01-19 15:20:44 +0300

    Создан класс Point с подклассами и разными характеристиками точки. Практика ООП. *Полиморфизм #11. Точки. Решение задачи.

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,33 +2,17 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
-        Shapesable[] figuras = {
-                new Circle(24, 12, 2),
-                new Square(11, 24, 2),
-                new Rectangle(12, 11, 3, 2),
-                new Square(33, 42, 12),
-                new Rectangle(44, 111, 21, 33),
-                new Circle(10, 2, 6),
-                new Circle(5, 1, 9)
+        Pointable[] points = {
+                new PointColor(new Point(6), "красного цвета"),
+                new PointTime(new Point(1,4), "11:00"),
+                new PointColor(new PointTime(new Point(12,4,5), "15:35"), "желтого цвета")
         };
 
-        for (Shapesable figura : figuras) {
-            System.out.println(figura);
+        for(Pointable point : points){
+            System.out.println(point);
         }
-
-        System.out.println(" ");
-        System.out.println("Общая площадь всех фигур: " + String.format("%.3f",  sumAllAreas(figuras)));
-
-    }
-    public static double sumAllAreas(Shapesable[] figuras){
-        double sum = 0;
-        for (Shapesable figura : figuras){
-            sum+= figura.getArea();
-        }
-        return sum;
     }
 }
 
-interface Shapesable {
-    double getArea();
+interface Pointable {
 }
