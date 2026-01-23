bced4a07d149d18d87ede90de10312cec1f152d5
Roman Grishin <grishin655@gmail.com>
2026-01-23 11:59:30 +0300

    Отредатирован класс Point. Добавлены и переопределены методы equals, hashCode и clone. Практика ООП. Класс Object #2. Сравнение точек. Решение задачи.

--- src/main/java/ru/courses/geometry/Point.java
+++ src/main/java/ru/courses/geometry/Point.java
@@ -1,61 +1,37 @@
 package ru.courses.geometry;
 
-import ru.courses.start.Pointable;
+import java.util.Objects;
 
-public class Point implements Pointable {
-    private final int[] point;
+public class Point implements Cloneable{
+    private int x,y;
 
-    public Point(int... point) {
-        if(point.length < 1 || point.length>3){
-            throw new IllegalArgumentException("Нужно ввести от 1 до 3 значений. Без выхода за эту границу");
-        }
-        this.point = point;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
     @Override
     public String toString() {
-        if (point.length == 1) {
-            return "Точка в координате " + point[0];
-        }
-        if (point.length == 2) {
-            return "Точка в координате {" + point[0] + ";" + point[1] + "}";
-        }
-        if (point.length == 3) {
-            return "Точка в координате {" + point[0] + ";" + point[1] + ";" + point[2] + "}";
-        }
-
-        return "Точка не верного формата";
-    }
-}
-
-class PointColor implements Pointable {
-    private final Pointable pointable;
-    private final String color;
-
-    public PointColor(Pointable pointable, String color) {
-        this.pointable = pointable;
-        this.color = color;
+        return "Point{" + x + ";" + y +
+                '}';
     }
 
     @Override
-    public String toString() {
-        return pointable +
-                ", " + color;
-    }
-}
-
-class PointTime implements Pointable {
-    private final Pointable pointable;
-    private final String time;
-
-    public PointTime(Pointable pointable, String time) {
-        this.pointable = pointable;
-        this.time = time;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
     }
 
     @Override
-    public String toString() {
-        return pointable +
-                ", в " + time;
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public Point clone() throws CloneNotSupportedException {
+        return (Point) super.clone();
     }
 }
+
--- src/main/java/ru/courses/start/MainApplication.java
+++ src/main/java/ru/courses/start/MainApplication.java
@@ -1,22 +1,22 @@
 package ru.courses.start;
 
-import ru.courses.geometry.Point;
-import com.stepup.proj.PointColor;
-import com.stepup.proj.PointTime;
+import ru.courses.geometry.*;
 
 public class MainApplication {
-    public static void main(String[] args) {
-        Pointable[] points = {
-                new PointColor(new Point(6), "красного цвета"),
-                new PointTime(new Point(1,4), "11:00"),
-                new PointColor(new PointTime(new Point(12,4,5), "15:35"), "желтого цвета")
-        };
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Point p1 = new Point(1,2);
+        Point p2 = new Point(1,2);
+        System.out.println(p1 == p2);
+        System.out.println(p1.equals(p2));
 
-        for(Pointable point : points){
-            System.out.println(point);
-        }
+        System.out.println("------------------------------");
+
+        Point p3 = p1.clone();
+        System.out.println(p3);
+        System.out.println(p2 == p3);
+        System.out.println(p2.equals(p3));
+
+
     }
 }
 
-public interface Pointable {
-}
